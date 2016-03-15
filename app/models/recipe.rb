70e48b808eea469acabffc7df5a0c0ff451f2228
dujar <dujar.coding@gmail.com>
2016-03-15 10:02:10 +0100

    TDD for chef and recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,9 @@
 class Recipe < ActiveRecord::Base
 
+  validates :name, presence: true, length: {minimum: 5, maximum: 100}
+  validates :summary, presence: true, length: {minimum: 10, maximum: 150}
+  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  #validates_associated :chef, :recipe
 
 
 end
